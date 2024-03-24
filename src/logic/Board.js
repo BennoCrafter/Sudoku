a02eb735cba234d01db738f6478dde4d70381251
benno <BennoCrafterDev@googlemail.com>
2024-03-24 17:23:01 +0100

    merged board

--- src/logic/Board.js
+++ src/logic/Board.js
@@ -3,16 +3,41 @@ export class Board {
 		this.values = []
 		this.createEmptyBoard()
 	}
-	setValue(row, col, val) {
-		this.values[row][col] = val;
+	setValue(rowIndex, colIndex, val) {
+		this.values[rowIndex][colIndex] = val
 	}
 
-	setValueAllowed(row, col, val) {
+	getSubBoard(rowIndex, colIndex) {
+		let ri = Math.floor(rowIndex/3) * 3 // first row index
+		let ci = Math.floor(colIndex/3) * 3 // first col index
+		let subBoard = [this.getValue(ri, ci), this.getValue(ri, ci+1), this.getValue(ri, ci+2),
+			this.getValue(ri+1, ci), this.getValue(ri+1, ci+1), this.getValue(ri+1, ci+2),
+			this.getValue(ri+2, ci), this.getValue(ri+2, ci+1), this.getValue(ri+2, ci+2)]
+		return subBoard
+	}
+
+	setValueAllowed(rowIndex, colIndex, val) {
+		if (!this.isEmpty(rowIndex, colIndex)) {
+			return false
+		}
+		// check whether row already contains the val
+		for (let colI = 0; colI < 9; colI++) {
+			if (this.getValue(rowIndex, colI) == val)
+				return false
+		}
+		// check whether row already contains the val
+		for (let rowI = 0; rowI < 9; rowI++) {
+			if (this.getValue(rowI, colIndex) == val)
+				return false
+		}
+		// check, whether the val is already in the sub-board
+		if (this.getSubBoard(rowIndex, colIndex).includes(val))
+			return false
 		return true;
 	}
 
-	getValue(row, col) {
-		return this.values[row][col];
+	getValue(rowIndex, colIndex) {
+		return this.values[rowIndex][colIndex]
 	}
 
 	createEmptyBoard() {
@@ -21,28 +46,26 @@ export class Board {
 		}
 	}
 
-  isEmpty(row, col) {
-    return this.getValue(row,col) === 0
-  }
+	isEmpty(rowIndex, colIndexl) {
+		return this.getValue(rowIndex, colIndexl) === 0
+	}
 
 	outputBoard() {
 		let hr = "----------------------------------------\n";
 		let st = hr + hr
 		for (let row = 0; row < 9; row++) {
 			for (let col = 0; col < 9; col++) {
-        if (this.isEmpty(row,col)) {
-          st += "|   " 
-        } else {
-          st += "| "+this.getValue(row, col) + " "
-        }
-      }
-      st += "|\n-------------------------------------\n"
+				if (col % 3 == 0) st += "|"
+				if (this.isEmpty(row, col)) {
+					st += "|   ";
+				} else {
+					st += "| " + this.getValue(row, col) + " ";
+				}
+			}
+			st += "|\n" + hr;
+			if ((row+1) % 3 == 0) st += hr
 		}
 		return st;
 	}
 }
 
-const board = new Board();
-board.setValue(0, 0, 1);
-board.setValue(8, 8, 1);
-console.log(board.outputBoard());

71d1a03d901dffba0d55d724014d77d6bfc6c3de
Joachim Baumeister <joachim.baumeister@denkbares.com>
2024-03-24 16:11:05 +0100

    Initial version of Board (data structure only)

--- src/logic/Board.js
+++ src/logic/Board.js
@@ -1,18 +1,47 @@
 class Board {
-  constructor(values=[]) {
-    this.values = values
-    this.createEmptyBoard()
-  }
-  setValue(x,y, val){
+	constructor() {
+		this.values = [];
+		this.createEmptyBoard();
+	}
+	setValue(row, col, val) {
+		this.values[row][col] = val;
+	}
 
+  setValueAllowed(row, col, val) {
+		return true
+	}
+
+	getValue(row, col) {
+		return this.values[row][col];
+	}
+
+	createEmptyBoard() {
+		for (let row = 0; row < 9; row++) {
+			this.values.push([0, 0, 0, 0, 0, 0, 0, 0, 0]);
+		}
+	}
+
+  isEmpty(row, col) {
+    return this.getValue(row,col) === 0
   }
-  createEmptyBoard(){
-    for (let row = 0; row < 9; row++){
-        this.values.push([0,0,0,0,0,0,0,0,0])
-    }
-  }
+
+	outputBoard() {
+		let st = "-------------------------------------\n";
+		for (let row = 0; row < 9; row++) {
+			for (let col = 0; col < 9; col++) {
+        if (this.isEmpty(row,col)) {
+          st += "|   " 
+        } else {
+          st += "| "+this.getValue(row, col) + " "
+        }
+      }
+      st += "|\n-------------------------------------\n"
+		}
+		return st;
+	}
 }
 
-
-const board = new Board([])
-console.log(board.values)
+const board = new Board();
+board.setValue(0, 0, 1);
+board.setValue(8, 8, 1);
+console.log(board.outputBoard());

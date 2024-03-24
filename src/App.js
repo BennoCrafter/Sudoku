c680d4d83562b87dda80b28a979d5ba4310bcb6e
benno <BennoCrafterDev@googlemail.com>
2024-03-24 17:16:07 +0100

    placing values on board

--- src/App.js
+++ src/App.js
@@ -3,103 +3,139 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import * as SplashScreen from "expo-splash-screen";
 import { useFonts } from "expo-font";
+import { Board } from "./logic/Board";
 
 SplashScreen.preventAutoHideAsync();
 
-const sudokuValues = [
-  [5, 3, null, null, 7, null, null, null, null],
-  [6, null, null, 1, 9, 5, null, null, null],
-  [null, 9, 8, null, null, null, null, 6, null],
-  [8, null, null, null, 6, null, null, null, 3],
-  [4, null, null, 8, null, 3, null, null, 1],
-  [7, null, null, null, 2, null, null, null, 6],
-  [null, 6, null, null, null, null, 2, 8, null],
-  [null, null, null, 4, 1, 9, null, null, 5],
-  [null, null, null, null, 8, null, null, 7, 9],
-];
-
 export default function App() {
-  const [selectedCell, setSelectedCell] = useState([]);
+	const [selectedCell, setSelectedCell] = useState([]);
+	const [board, setBoard] = useState(new Board());
+  const [, updateState] = useState();
+  const forceUpdate = useCallback(() => updateState({}), []);
+	board.setValue(0, 1, 1);
 
-  const [fontsLoaded, fontError] = useFonts({
-    roboto: require("../assets/Roboto-Bold.ttf"),
-  });
-  const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded || fontError) {
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded, fontError]);
+	const [fontsLoaded, fontError] = useFonts({
+		roboto: require("../assets/Roboto-Bold.ttf"),
+	});
 
-  useEffect(() => {
-    onLayoutRootView();
-  }, [onLayoutRootView]);
+	const onLayoutRootView = useCallback(async () => {
+		if (fontsLoaded || fontError) {
+			await SplashScreen.hideAsync();
+		}
+	}, [fontsLoaded, fontError]);
 
-  if (!fontsLoaded && !fontError) {
-    return null;
-  }
-  
-  const renderSudokuGrid = () => {
-    return sudokuValues.map((row, rowIndex) => (
-      <View key={rowIndex} style={styles.row}>
-        {row.map((colum, columIndex) => (
-          <TouchableOpacity
-            key={`${rowIndex}-${columIndex}`}
-            style={[
-              styles.cell,
-              selectedCell[0] === rowIndex && selectedCell[1] === columIndex
-                ? styles.selectedCellStyle
-                : null,
-            ]}
-            onPress={() => setSelectedCell([rowIndex, columIndex])}
-          >
-            <Text style={styles.cellItem}>{colum || ""}</Text>
-          </TouchableOpacity>
-        ))}
-      </View>
-    ));
-  };
+	useEffect(() => {
+		onLayoutRootView();
+	}, [onLayoutRootView]);
 
-  return (
-    <View style={styles.container}>
-      {/* Sudoku Grid */}
-      <View style={styles.grid}>{renderSudokuGrid()}</View>
-    </View>
-  );
+	if (!fontsLoaded && !fontError) {
+		return null;
+	}
+
+	const renderSudokuGrid = () => {
+		return board.values.map((row, rowIndex) => (
+			<View key={rowIndex} style={styles.row}>
+				{row.map((colum, columIndex) => (
+					<TouchableOpacity
+						key={`${rowIndex}-${columIndex}`}
+						style={[
+							styles.cell,
+							selectedCell[0] === rowIndex &&
+							selectedCell[1] === columIndex
+								? styles.selectedCellStyle
+								: null,
+						]}
+						onPress={() => setSelectedCell([rowIndex, columIndex])}
+					>
+						<Text style={styles.cellItem}>
+							{!board.isEmpty(rowIndex, columIndex)
+								? board.getValue(rowIndex, columIndex)
+								: ""}
+						</Text>
+					</TouchableOpacity>
+				))}
+			</View>
+		));
+	};
+
+	const renderNumbers = () => {
+		const numbers = [];
+		for (let i = 1; i <= 9; i++) {
+			numbers.push(
+				<TouchableOpacity
+					key={i}
+					style={styles.numberButton}
+					onPress={() => {
+						board.setValue(selectedCell[0], selectedCell[1], i);
+            forceUpdate()
+					}}
+				>
+					<Text style={styles.numberText}>{i}</Text>
+				</TouchableOpacity>
+			);
+		}
+		return numbers;
+	};
+
+	return (
+		<View style={styles.container}>
+			{/* Sudoku Grid */}
+			<View style={styles.grid}>{renderSudokuGrid()}</View>
+			<View style={styles.numberSelector}>{renderNumbers()}</View>
+		</View>
+	);
 }
 
 // Function to render the Sudoku grid
 
 // Styles
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  grid: {
-    borderWidth: 1,
-    borderColor: "black",
-  },
-  row: {
-    flexDirection: "row",
-  },
-  cell: {
-    width: 40,
-    height: 40,
-    borderWidth: 1,
-    borderColor: "black",
-    textAlign: "center",
+	container: {
+		flex: 1,
+		backgroundColor: "#F5F5F5",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	grid: {
+		borderWidth: 1,
+		borderColor: "black",
+	},
+	row: {
+		flexDirection: "row",
+	},
+	cell: {
+		width: 40,
+		height: 40,
+		borderWidth: 1,
+		borderColor: "black",
+		textAlign: "center",
 
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  cellItem: {
-    fontSize: 20,
-    fontFamily: "roboto",
-  },
-  selectedCellStyle: {
-    borderColor: "blue",
-    borderWidth: 3,
-  },
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	cellItem: {
+		fontSize: 20,
+		fontFamily: "roboto",
+	},
+	selectedCellStyle: {
+		borderColor: "blue",
+		borderWidth: 3,
+	},
+	numberSelector: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		paddingHorizontal: 20,
+		marginTop: 20,
+	},
+	numberButton: {
+		backgroundColor: "#D4CBCE",
+		padding: 10,
+		borderRadius: 5,
+		marginRight: "1%",
+	},
+	numberText: {
+		fontSize: 18,
+		fontWeight: "bold",
+		fontFamily: "roboto",
+	},
 });
--- src/logic/Board.js
+++ src/logic/Board.js
@@ -1,43 +1,18 @@
-class Board {
+export class Board {
 	constructor() {
 		this.values = []
 		this.createEmptyBoard()
 	}
-	setValue(rowIndex, colIndex, val) {
-		this.values[rowIndex][colIndex] = val
+	setValue(row, col, val) {
+		this.values[row][col] = val;
 	}
 
-	getSubBoard(rowIndex, colIndex) {
-		let ri = Math.floor(rowIndex/3) * 3 // first row index
-		let ci = Math.floor(colIndex/3) * 3 // first col index
-		let subBoard = [this.getValue(ri, ci), this.getValue(ri, ci+1), this.getValue(ri, ci+2),
-			this.getValue(ri+1, ci), this.getValue(ri+1, ci+1), this.getValue(ri+1, ci+2),
-			this.getValue(ri+2, ci), this.getValue(ri+2, ci+1), this.getValue(ri+2, ci+2)]
-		return subBoard
-	}
-
-	setValueAllowed(rowIndex, colIndex, val) {
-		if (!this.isEmpty(rowIndex, colIndex)) {
-			return false
-		}
-		// check whether row already contains the val
-		for (let colI = 0; colI < 9; colI++) {
-			if (this.getValue(rowIndex, colI) == val)
-				return false
-		}
-		// check whether row already contains the val
-		for (let rowI = 0; rowI < 9; rowI++) {
-			if (this.getValue(rowI, colIndex) == val)
-				return false
-		}
-		// check, whether the val is already in the sub-board
-		if (this.getSubBoard(rowIndex, colIndex).includes(val))
-			return false
+	setValueAllowed(row, col, val) {
 		return true;
 	}
 
-	getValue(rowIndex, colIndex) {
-		return this.values[rowIndex][colIndex]
+	getValue(row, col) {
+		return this.values[row][col];
 	}
 
 	createEmptyBoard() {
@@ -46,27 +21,28 @@ class Board {
 		}
 	}
 
-	isEmpty(rowIndex, colIndexl) {
-		return this.getValue(rowIndex, colIndexl) === 0
-	}
+  isEmpty(row, col) {
+    return this.getValue(row,col) === 0
+  }
 
 	outputBoard() {
 		let hr = "----------------------------------------\n";
 		let st = hr + hr
 		for (let row = 0; row < 9; row++) {
 			for (let col = 0; col < 9; col++) {
-				if (col % 3 == 0) st += "|"
-				if (this.isEmpty(row, col)) {
-					st += "|   ";
-				} else {
-					st += "| " + this.getValue(row, col) + " ";
-				}
-			}
-			st += "|\n" + hr;
-			if ((row+1) % 3 == 0) st += hr
+        if (this.isEmpty(row,col)) {
+          st += "|   " 
+        } else {
+          st += "| "+this.getValue(row, col) + " "
+        }
+      }
+      st += "|\n-------------------------------------\n"
 		}
 		return st;
 	}
 }
 
-module.exports = { Board }
+const board = new Board();
+board.setValue(0, 0, 1);
+board.setValue(8, 8, 1);
+console.log(board.outputBoard());
